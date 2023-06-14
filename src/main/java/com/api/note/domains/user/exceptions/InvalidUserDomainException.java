c8cfd8cb9821f31800ff5a6d20994fafe5591955
lucasrluz <lucasr.luzbr@gmail.com>
2023-06-14 18:43:26 -0300

    feat: criado exception para UserDomain inválido

---
+++ src/main/java/com/api/note/domains/user/exceptions/InvalidUserDomainException.java
@@ -0,0 +1,7 @@
+package com.api.note.domains.user.exceptions;
+
+public class InvalidUserDomainException extends Exception {
+    public InvalidUserDomainException(String message) {
+        super(message);
+    }
+}
