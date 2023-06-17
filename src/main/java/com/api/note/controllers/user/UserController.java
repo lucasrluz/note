c7a7854484f2538fc4131e265129d16ee6d3b190
lucasrluz <lucasr.luzbr@gmail.com>
2023-06-17 19:56:11 -0300

    feat: criado UserController com método para salvar novo usuário no sistema

---
+++ src/main/java/com/api/note/controllers/user/UserController.java
@@ -0,0 +1,33 @@
+package com.api.note.controllers.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.note.dtos.user.UserDTOSaveRequest;
+import com.api.note.dtos.user.UserDTOSaveResponse;
+import com.api.note.services.user.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody UserDTOSaveRequest userDTOSaveRequest) {
+        try {
+            UserDTOSaveResponse saveUserResponse = this.userService.save(userDTOSaveRequest);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(saveUserResponse);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+}
