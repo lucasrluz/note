cdbb0d2153cb8ca44c83d29ef00ebd3346d0ba2c
lucasrluz <lucasr.luzbr@gmail.com>
2023-06-17 20:37:56 -0300

    feat: criado exception para NoteDomain inválido

---
+++ src/main/java/com/api/note/domains/note/exceptions/InvalidNoteDomainException.java
@@ -0,0 +1,7 @@
+package com.api.note.domains.note.exceptions;
+
+public class InvalidNoteDomainException extends Exception {
+    public InvalidNoteDomainException(String message) {
+        super(message);
+    }
+}
