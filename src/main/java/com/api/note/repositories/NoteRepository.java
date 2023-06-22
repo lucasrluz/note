c363fcc14d1bc33bebb024938cac2ed562e430c2
lucasrluz <lucasr.luzbr@gmail.com>
2023-06-22 14:37:59 -0300

    feat: adicionado método no NoteRepository para busca de nota pelo título e pelo usuário

--- src/main/java/com/api/note/repositories/NoteRepository.java
+++ src/main/java/com/api/note/repositories/NoteRepository.java
@@ -1,13 +1,16 @@
 package com.api.note.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.api.note.models.NoteModel;
+import com.api.note.models.UserModel;
 
 @Repository
 public interface NoteRepository extends JpaRepository<NoteModel, UUID> {
     public boolean existsByTitle(String title);
+    public List<NoteModel> findByTitleAndUserModel(String title, UserModel userModel);
 }
