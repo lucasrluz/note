575fdc159c6934c417f8c28fbaf3b5941cc5370b
lucasrluz <lucasr.luzbr@gmail.com>
2023-06-27 16:15:17 -0300

    test: criado testes de integração para busca de nota pelo userId no NoteController

---
+++ src/test/java/com/api/note/integration/controllers/note/NoteControllerFindByUserIdTests.java
@@ -0,0 +1,98 @@
+package com.api.note.integration.controllers.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.CoreMatchers.is;
+
+import com.api.note.models.NoteModel;
+import com.api.note.models.UserModel;
+import com.api.note.repositories.NoteRepository;
+import com.api.note.repositories.UserRepository;
+import com.api.note.utils.builders.note.NoteModelBuilder;
+import com.api.note.utils.builders.user.UserModelBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NoteControllerFindByUserIdTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public static String asJsonString(final Object obj) {
+        try {
+          return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void esperoQueRetorneUmCodigoDeStatus201ComAsNotasDoUsuario() throws Exception {
+        UserModel userModel = UserModelBuilder.createWithValidData();
+        UserModel saveUserModelResponse = this.userRepository.save(userModel);
+
+        this.userRepository.save(userModel);
+
+        NoteModel firstNoteModel = NoteModelBuilder.createWithValidData();
+        firstNoteModel.userModel = userModel;
+        
+        NoteModel secondNoteModel = NoteModelBuilder.createWithValidData();
+        secondNoteModel.userModel = userModel;
+
+        this.noteRepository.save(firstNoteModel);
+        this.noteRepository.save(secondNoteModel);
+        
+        String url = "/note/" + saveUserModelResponse.userId.toString();
+
+        this.mockMvc.perform(
+            get(url))
+            .andExpect(status().isOk())
+            .andExpect(content().json("[{'title':'Foo Bar', 'content':'Foo bar'},{'title':'Foo Bar', 'content':'Foo bar'}]"));
+    
+		this.noteRepository.deleteAll();
+		this.userRepository.deleteAll();
+	}
+
+	@Test
+    public void esperoQueRetorneUmCodigoDeStatus404ComUmaMensagemDeUsuaioNaoEncontrado() throws Exception {        
+        String url = "/note/" + UUID.randomUUID().toString();
+
+        this.mockMvc.perform(
+            get(url))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$", is("Error: usuário não encontrado")));
+	}
+
+	@Test
+    public void esperoQueRetorneUmCodigoDeStatus404ComUmaMensagemDeNotasNaoEncontradas() throws Exception {
+        UserModel userModel = UserModelBuilder.createWithValidData();
+        UserModel saveUserModelResponse = this.userRepository.save(userModel);
+
+        this.userRepository.save(userModel);
+        
+        String url = "/note/" + saveUserModelResponse.userId.toString();
+
+        this.mockMvc.perform(
+            get(url))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$", is("Error: Este usuário não possui notas criadas")));
+    
+		this.userRepository.deleteAll();
+	}
+}
