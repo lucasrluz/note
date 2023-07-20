1bbe0cc27ebd4d1b07079df51f67971ef8d58fa7
lucasrluz <lucasr.luzbr@gmail.com>
2023-07-20 11:17:45 -0300

    feat: criado AuthService com método de login

---
+++ src/main/java/com/api/note/services/auth/AuthService.java
@@ -0,0 +1,44 @@
+package com.api.note.services.auth;
+
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.api.note.dtos.login.LoginDTORequest;
+import com.api.note.dtos.login.LoginDTOResponse;
+import com.api.note.models.UserModel;
+import com.api.note.repositories.UserRepository;
+import com.api.note.services.util.BadRequestException;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import at.favre.lib.crypto.bcrypt.BCrypt.Result;
+
+@Service
+public class AuthService {
+    private UserRepository userRepository;
+    private JwtService jwtService;
+
+    public AuthService(UserRepository userRepository, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+    }
+
+    public LoginDTOResponse login(@RequestBody LoginDTORequest loginDTORequest) throws BadRequestException {
+        Optional<UserModel> findUserModelByEmail = this.userRepository.findByEmail(loginDTORequest.email);
+
+        if (findUserModelByEmail.isEmpty()) {
+            throw new BadRequestException("Error: E-mail ou senha inválido");
+        }
+
+        Result validatePasswordResponse = BCrypt.verifyer().verify(
+            loginDTORequest.password.toCharArray(),
+            findUserModelByEmail.get().password
+        );
+    
+        if (!validatePasswordResponse.verified) {
+            throw new BadRequestException("Error: E-mail ou senha inválido");
+        }
+
+        String jwt = jwtService.generateJwt(loginDTORequest.email);
+
+        return new LoginDTOResponse(jwt);
+    }
+}
