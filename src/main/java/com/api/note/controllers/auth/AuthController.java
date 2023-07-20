19757f32dd14dadd9366707dfd3df2c98b614f23
lucasrluz <lucasr.luzbr@gmail.com>
2023-07-20 16:36:59 -0300

    feat: criado AuthController com rota de login

---
+++ src/main/java/com/api/note/controllers/auth/AuthController.java
@@ -0,0 +1,33 @@
+package com.api.note.controllers.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.note.dtos.login.LoginDTORequest;
+import com.api.note.dtos.login.LoginDTOResponse;
+import com.api.note.services.auth.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody LoginDTORequest loginDTORequest) {
+        try {
+            LoginDTOResponse loginDTOResponse = this.authService.login(loginDTORequest);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(loginDTOResponse);
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+}
